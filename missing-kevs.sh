1e8d469c26975da7812c0d62100520ad3d25cc3c
Tod Beardsley <todb@cisa.dhs.gov>
2025-01-02 13:24:53 -0600

    Technically CVEs can be real long
    Signed-off-by: Tod Beardsley <todb@cisa.dhs.gov>

--- missing-kevs.sh
+++ missing-kevs.sh
@@ -2,7 +2,7 @@
 # Usage: check infile.txt for all mentioned CVEs. Then get the latest KEV, and check to see
 # if there are any mentioned in infile.txt isn't on KEV.
 
-grep -Eo 'CVE-[0-9]{4}-[0-9]{4,7}' infile.txt | sort | uniq | \
+grep -Eo 'CVE-[0-9]{4}-[0-9]{4,19}' infile.txt | sort | uniq | \
   comm -23 - <(curl -s https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json | \
-  grep -Eo 'CVE-[0-9]{4}-[0-9]{4,7}' | sort | uniq) | \
+  grep -Eo 'CVE-[0-9]{4}-[0-9]{4,19}' | sort | uniq) | \
   awk '{print "[*] Missing: " $1 " not on KEV!"}'
